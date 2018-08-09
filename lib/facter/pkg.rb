e327c974325c100c2276f250dd334bda24953052
Ian Gibbs <realflash.uk@googlemail.com>
2018-08-09 14:21:32 +0100

    First version import

---
+++ lib/facter/pkg.rb
@@ -0,0 +1,17 @@
+# Modified code originally written by 
+# Cody Herriges <cody@puppetlabs.com>
+#
+# Collects and creates a fact for every package installed on the system and
+# returns that package's version as the fact value.  Useful for doing package
+# inventory and making decisions based on installed package versions.
+
+require 'facter/util/pkg'
+
+Facter::Util::Pkg.package_list.each do |key, value|
+  Facter.add(:"pkg_#{key}") do
+    confine :operatingsystem => %w{CentOS Fedora Redhat Debian Ubuntu Solaris LinuxMint}
+    setcode do
+      value
+    end
+  end
+end
