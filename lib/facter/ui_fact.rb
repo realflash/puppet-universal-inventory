fd21c243d13bd878122adccba5a8a071d6f894af
Ian Gibbs <realflash.uk@googlemail.com>
2018-08-10 11:20:59 +0100

    Working for RPM, hopefully. More OS names added.

--- lib/facter/ui_fact.rb
+++ lib/facter/ui_fact.rb
@@ -11,34 +11,36 @@ module UIDiscover
     packages = []
     case Facter.value(:operatingsystem)
     when 'Debian', 'Ubuntu', 'LinuxMint'
-	  puts "2"
       command = 'dpkg-query -W'
       packages = []
       Facter::Util::Resolution.exec(command).each_line do |pkg|
-        packages << pkg.chomp.split("\t")
+        pkg_parts = pkg.chomp.split("\t")
+        packages << { "name" => pkg_parts[0], "installed_version" => pkg_parts[1] }
       end
-    when 'CentOS', 'RedHat', 'Fedora'
+    when 'CentOS', 'RedHat', 'Fedora', 'Amazon', 'OracleLinux', 'Scientific', 'SLES'
       command = 'rpm -qa --qf %{NAME}"\t"%{VERSION}-%{RELEASE}"\n"'
       packages = []
       Facter::Util::Resolution.exec(command).each_line do |pkg|
-        packages << pkg.chomp.split("\t")
+        pkg_parts = pkg.chomp.split("\t")
+        packages << { "name" => pkg_parts[0], "installed_version" => pkg_parts[1] }
       end
     # I don't have any Solaris boxes to test this on but if someone
     # wants to do that I don't mind listing it as a supported platform
-    when 'Solaris'
-      command = 'pkginfo -x'
-      combined = ''
-      packages = []
-      Facter::Util::Resolution.exec(command).each_line do |line|
-        if line =~ /^\w/
-          combined << line.chomp
-        else
-          combined << line
-        end
-      end
-      combined.each_line do |pkg|
-        packages << pkg.chomp.scan(/^(\S+).*\s(\d.*)/)[0]
-      end
+	# Needs updating to provide response in structured format
+    #~ when 'Solaris'
+      #~ command = 'pkginfo -x'
+      #~ combined = ''
+      #~ packages = []
+      #~ Facter::Util::Resolution.exec(command).each_line do |line|
+        #~ if line =~ /^\w/
+          #~ combined << line.chomp
+        #~ else
+          #~ combined << line
+        #~ end
+      #~ end
+      #~ combined.each_line do |pkg|
+        #~ packages << pkg.chomp.scan(/^(\S+).*\s(\d.*)/)[0]
+      #~ end
     end
     return packages
   end
